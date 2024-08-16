451a4abff9fe56a711f48af6a2f3e0743c61cb1e
OutstandingBillNeal <billtadpole@yahoo.com>
2024-08-16 18:58:05 +1200

    Create the model and controller.

---
+++ CaaCodingChallenge/FlightsApi/Controllers/FlightsController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace FlightsApi.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FlightsController : ControllerBase
+    {
+        // GET: api/<FlightsController>
+        [HttpGet]
+        public IEnumerable<Flight> Get()
+        {
+            return [GetDummyFlight()];
+        }
+
+        // GET api/<FlightsController>/5
+        [HttpGet("{id}")]
+        public Flight Get(int id)
+        {
+            return GetDummyFlight();
+        }
+
+        // POST api/<FlightsController>
+        [HttpPost]
+        public Flight Post([FromBody] string value)
+        {
+            return GetDummyFlight();
+        }
+
+        // PUT api/<FlightsController>/5
+        [HttpPut("{id}")]
+        public Flight Put(int id, [FromBody] string value)
+        {
+            return GetDummyFlight();
+        }
+
+        // DELETE api/<FlightsController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+
+        private Flight GetDummyFlight()
+        {
+            return new Flight { Id = 1, Airline = "a", ArrivalAirport = "b", ArrivalTime = DateTimeOffset.Now, DepartureAirport = "c", DepartureTime = DateTimeOffset.Now, FlightNumber = "d", Status = FlightStatus.Scheduled };
+        }
+    }
+}
---
+++ CaaCodingChallenge/FlightsApi/FlightStatus.cs
@@ -0,0 +1,11 @@
+﻿namespace FlightsApi
+{
+    public enum FlightStatus
+    {
+        Scheduled,
+        Delayed,
+        Cancelled,
+        InAir,
+        Landed
+    }
+}
