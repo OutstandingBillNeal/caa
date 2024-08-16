8aa56033b8a2c47de2228a0823faf072e2c65fd2
OutstandingBillNeal <billtadpole@yahoo.com>
2024-08-16 15:33:19 +1200

    Add authorisation to the default controller (having checked it results in 401).

--- CaaCodingChallenge/FlightsApi/Controllers/WeatherForecastController.cs
+++ CaaCodingChallenge/FlightsApi/Controllers/WeatherForecastController.cs
@@ -1,7 +1,9 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FlightsApi.Controllers
 {
+    [Authorize]
     [ApiController]
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
--- CaaCodingChallenge/FlightsApi/Program.cs
+++ CaaCodingChallenge/FlightsApi/Program.cs
@@ -1,18 +1,38 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using Ardalis.GuardClauses;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// adding jwt auth
+var jwtIssuer = builder.Configuration["Jwt:Issuer"];
+var jwtKey = builder.Configuration["Jwt:Key"];
+Guard.Against.Null(jwtIssuer);
+Guard.Against.Null(jwtKey);
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            // define which claims should be checked
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = jwtIssuer,
+            ValidAudience = jwtIssuer,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtKey))
+        };
+    });
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -20,7 +40,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseRouting();
+app.UseAuthorization();
 app.UseAuthorization();
 
 app.MapControllers();
