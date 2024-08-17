56f31aaaa885c965b2a8e45b8d5f7d7ee99b5c83
OutstandingBillNeal <billtadpole@yahoo.com>
2024-08-17 12:55:14 +1200

    Add CRUD tests for DbContext.

--- CaaCodingChallenge/FlightsDataTests/DbContextTests.cs
+++ CaaCodingChallenge/FlightsDataTests/DbContextTests.cs
@@ -39,6 +39,49 @@ namespace FlightsDataTests
             Assert.NotEqual(0, flight.Id);
         }
 
+        [Fact]
+        public void Flights_can_be_deleted()
+        {
+            // Arrange
+            var sut = new FlightsContext();
+            var flight = GetFlight();
+            sut.Flights.Add(flight);
+            sut.SaveChanges();
+            var flightId = flight.Id;
+            var flightRetrievedBeforeDeletion = sut.Flights.FirstOrDefault(f => f.Id == flightId);
+            Assert.NotNull(flightRetrievedBeforeDeletion);
+
+            // Act
+            sut.Flights.Remove(flight);
+            sut.SaveChanges();
+
+            // Assert
+            var flightRetrievedAfterDeletion = sut.Flights.FirstOrDefault(f => f.Id == flightId);
+            Assert.Null(flightRetrievedAfterDeletion);
+        }
+
+        [Fact]
+        public void Flights_can_be_updated()
+        {
+            // Arrange
+            var sut = new FlightsContext();
+            var flight = GetFlight();
+            var initialFlightNumber = Guid.NewGuid().ToString();
+            flight.FlightNumber = initialFlightNumber;
+            sut.Flights.Add(flight);
+            sut.SaveChanges();
+            var flightId = flight.Id;
+
+            // Act
+            var updatedFlightNumber = Guid.NewGuid().ToString();
+            flight.FlightNumber = updatedFlightNumber;
+            sut.SaveChanges();
+
+            // Assert
+            var flightRetrievedAfterUpdate = sut.Flights.First(f => f.Id == flightId);
+            Assert.Equal(updatedFlightNumber, flightRetrievedAfterUpdate.FlightNumber);
+        }
+
         private Flight GetFlight()
         {
             return new Flight
